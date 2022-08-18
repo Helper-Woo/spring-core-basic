7104e8578b1f96ecdb1aa228a7b5d43d93972e9e
jaewoo <helperwoo.ko@gmail.com>
2022-08-18 14:18:24 +0900

    탐색 위치와 기본 스캔 대상
    @ComponentScan 옵션, 그리고 @SpringBootApplication에 포함
    @Controller, @Service, @Repository, @Configuration도 @Component 포함

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,10 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 /**
- * ComponentScan은 Component 어노테이션으로 붙은 클래스를 스캔해서 Bean으로 등록
+ * @ComponentScan: @Component 어노테이션으로 붙은 클래스를 스캔해서 @Bean으로 등록
+ * basePackages: 지정된 패키지를 스캔 위치로 지정
+ * basePackageClasses: 클래스의 패키지를 스캔 위치로 지정
+ * excludeFilters: 스캔 제외 대상 클래스
+ *
+ * basePackages, basePackageClasses 미지정시 해당 @ComponentScan 클래스의 패키지를 위치로 지정
+ * Spring Boot는 CoreApplication의 @SpringBootApplication 안에 @ComponentScan이 있어 자동으로 스캔위치가 지정된다
  */
 @Configuration
 @ComponentScan(
+        basePackages = {"hello.core.discount", "hello.core.member", "hello.core.order"},
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
